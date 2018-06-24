ce243ec7bffee15b72745f17ee81ba233b03220c
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-06-24 12:13:26 +0200

    a11y improvements

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,17 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <input
-        className='pa3 ba b--green bg-lightest-blue'
-        type='search'
-        placeholder='search robots'
-        onChange={searchChange}
-      />
+      <form>
+        <label for="robo-search">Robo-Search
+        <input
+            id="robo-search"
+            className='pa3 ba b--green bg-lightest-blue'
+            type='search'
+            placeholder='search robots'
+            onChange={searchChange}
+          />
+        </label>
+      </form>
     </div>
   );
 }
