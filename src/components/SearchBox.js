1e98cb4ec8e742cc8830bc56e2917176da11b7d8
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-06-24 12:38:08 +0200

    TWEAK: use aria-label in SearchBox component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,17 +3,14 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <form>
-        <label for="robo-search">Robo-Search
-        <input
-            id="robo-search"
-            className='pa3 ba b--green bg-lightest-blue'
-            type='search'
-            placeholder='search robots'
-            onChange={searchChange}
-          />
-        </label>
-      </form>
+      <input
+        aria-label="Search robots"
+        className='pa3 ba b--green bg-lightest-blue'
+        id="robo-search"
+        onChange={searchChange}
+        placeholder='search robots'
+        type='search'
+      />
     </div>
   );
 }
