8f5e27c03d5fa8b5c7fc929852e50dfcf746fc95
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-06-26 17:08:57 +0200

    CHORE: wrap up structure update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import thunk from 'redux-thunk';
 import App from './containers/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 if (process.env.NODE_ENV !== 'production') {
     const { whyDidYouUpdate } = require('why-did-you-update');
